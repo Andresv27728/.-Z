0403cd4dd2f7679071041d7153d4a1b56529a5b9
Andresv27728 <andrescamilovallejogomez701@gmail.com>
2025-08-12 22:22:20 -0500

    Update owner-sf.js

--- plugins/owner-sf.js
+++ plugins/owner-sf.js
@@ -1,14 +1,17 @@
 let handler = async (m, { text, usedPrefix, command }) => {
-    if (!text) throw `uhm.. teksnya mana?\n\npenggunaan:\n${usedPrefix + command} <teks>\n\ncontoh:\n${usedPrefix + command} plugins/menu.js`
-    if (!m.quoted.text) throw `balas pesan nya!`
-    let path = `${text}`
-    await require('fs').writeFileSync(path, m.quoted.text)
-    m.reply(`tersimpan di ${path}`)
-}
-handler.help = ['sf'].map(v => v + ' <teks>')
-handler.tags = ['owner']
-handler.command = /^sf$/i
+    if (!text) throw `📂 *Falta la ruta del archivo*\n\n💡 Uso:\n${usedPrefix + command} <ruta>\n\nEjemplo:\n${usedPrefix + command} plugins/menu.js`;
+    if (!m.quoted.text) throw `💬 *Responde a un mensaje de texto para guardarlo como archivo*`;
 
-handler.rowner = true
+    let path = `${text}`;
+    await require('fs').writeFileSync(path, m.quoted.text);
 
-module.exports = handler
+    m.reply(`✅ Archivo guardado correctamente en:\n📄 *${path}*`);
+};
+
+handler.help = ['sf <ruta>'];
+handler.tags = ['owner'];
+handler.command = /^sf$/i;
+
+handler.rowner = true;
+
+module.exports = handler;
