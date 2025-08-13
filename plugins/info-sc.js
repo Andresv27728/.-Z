6d7e7d6e1f33e0e31c85ab484b09d92716c8c17c
Andresv27728 <andrescamilovallejogomez701@gmail.com>
2025-08-12 21:14:40 -0500

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -1,7 +1,8 @@
 let handler = async (m, { conn }) => {
 let ye = `@${m.sender.split`@`[0]}`
 let esce = `
-Hai ${ye} Bot Ini Menggunakan Script :\n• https://github.com/BOTCAHX/RTXZY-MD
+Hai ${ye} Este bot nada por los mares usando un script especial~ 🫧  
+¡Listo para ayudarte, capitán~!:\n• https://github.com/Andresv27728/GawrGura.git
 `
 m.reply(esce)
 }
