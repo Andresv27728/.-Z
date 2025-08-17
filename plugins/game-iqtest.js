64ece7c1b088c6f581cbadd5a578c394302792af
Andresv27728 <andrescamilovallejogomez701@gmail.com>
2025-08-17 14:59:25 -0500

    Update game-iqtest.js

--- plugins/game-iqtest.js
+++ plugins/game-iqtest.js
@@ -1,48 +1,87 @@
 let handler  = async (m, { conn }) => {
-  conn.reply(m.chat,`${pickRandom(global.iq)}`, m)
-}
-handler.help = ['iqtest']
-handler.tags = ['game']
-handler.command = /^(iqtest)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
+    // Mensajes de introducción aleatorios
+    const mensajesInicio = [
+        '🦈💙 ¡Vamos a medir tu intelecto submarino, capitán! 💙🦈',
+        '🌊🐟 ¡El tiburoncito Gura está calculando tu IQ! 🐟🌊',
+        '🐠✨ ¡Sumergiéndonos en tu cerebro, tiburoncito! ✨🐠',
+        '💦🦈 ¡Tiempo de ver cuán inteligente eres! 💦🦈',
+        '🦈🌟 ¡Preparando los cálculos acuáticos! 🌟🦈',
+        '🐟💙 ¡El océano del conocimiento te espera! 💙🐟',
+        '🌊✨ ¡Gura está a punto de revelarte tu IQ! ✨🌊',
+        '🦈🐠 ¡Atento! Comenzando la evaluación 🐠🦈',
+        '💙🐋 ¡Calculando con estilo submarino! 💙🐋',
+        '🐠🦈 ¡Que los números fluyan como el océano! 🦈🐠'
+    ];
+    let inicio = mensajesInicio[Math.floor(Math.random() * mensajesInicio.length)];
+    await conn.reply(m.chat, inicio, m);
 
-handler.admin = false
-handler.botAdmin = false
+    // Elegir IQ aleatorio
+    let resultado = pickRandom(global.iq);
 
-handler.fail = null
+    // Bordes decorativos estilo océano con peces y burbujas
+    const bordes = [
+        '🌊🐠 ~~~~~~~~~~~~~~~~~~~~~~ 🌊🐠\n💙 %resultado 💙\n🌊🐟 ~~~~~~~~~~~~~~~~~~~~~~ 🌊🐟',
+        '🐠💦 ~~~~~~~~🌊~~~~~~~~ 🐟💦\n💙 %resultado 💙\n🐟🌊 ~~~~~~~~💦~~~~~~~~ 🐠🌊',
+        '🦈🌊 -------------------- 🦈🌊\n💙 %resultado 💙\n🌊🦈 -------------------- 🌊🦈',
+        '🐟🌊 ~~~~~~~~~~~~~~~~~~~ 🌊🐠\n💙 %resultado 💙\n🌊🐠 ~~~~~~~~~~~~~~~~~~~ 🐟🌊',
+        '💦🦈 -------------------- 🦈💦\n💙 %resultado 💙\n💦🦈 -------------------- 🦈💦',
+        '🌊🐠 ~~~~💦~~~💙~~~💦~~~~ 🌊🐠\n💙 %resultado 💙\n🌊🐟 ~~~~💦~~~💙~~~💦~~~~ 🌊🐟',
+        '🐠🌊 ~~~~🐟~~~💦~~~🐟~~~~ 🌊🐠\n💙 %resultado 💙\n🌊🐟 ~~~~🐠~~~💦~~~🐠~~~~ 🌊🐟',
+        '🦈💦 ~~~~~~~~~~~~~~~~~~~~ 💦🦈\n💙 %resultado 💙\n🦈💦 ~~~~~~~~~~~~~~~~~~~~ 💦🦈',
+        '🐟🌊 ~~~💙~~~🐠~~~💦~~~ 🌊🐟\n💙 %resultado 💙\n🌊🐠 ~~~💦~~~🐟~~~💙~~~ 🌊🐠',
+        '🌊🦈 -------------------- 🦈🌊\n💙 %resultado 💙\n🌊🦈 -------------------- 🦈🌊'
+    ];
+    let borde = bordes[Math.floor(Math.random() * bordes.length)];
 
-module.exports = handler
+    // Reemplazar %resultado en el borde
+    let mensajeDecorado = borde.replace('%resultado', resultado);
+
+    // Enviar mensaje decorado
+    await conn.reply(m.chat, mensajeDecorado, m);
+};
+
+handler.help = ['iqtest'];
+handler.tags = ['game'];
+handler.command = /^(iqtest)$/i;
+
+handler.owner = false;
+handler.mods = false;
+handler.premium = false;
+handler.group = false;
+handler.private = false;
+
+handler.admin = false;
+handler.botAdmin = false;
+handler.fail = null;
+
+module.exports = handler;
 
 function pickRandom(list) {
-  return list[Math.floor(list.length * Math.random())]
+    return list[Math.floor(Math.random() * list.length)];
 }
 
 global.iq = [
-'IQ Anda Sebesar : 1',
-'IQ Anda Sebesar : 14',
-'IQ Anda Sebesar : 23',
-'IQ Anda Sebesar : 35',
-'IQ Anda Sebesar : 41',
-'IQ Anda Sebesar : 50',
-'IQ Anda Sebesar : 67',
-'IQ Anda Sebesar : 72',
-'IQ Anda Sebesar : 86',
-'IQ Anda Sebesar : 99',
-'IQ Anda Sebesar : 150',
-'IQ Anda Sebesar : 340',
-'IQ Anda Sebesar : 423',
-'IQ Anda Sebesar : 500',
-'IQ Anda Sebesar : 676',
-'IQ Anda Sebesar : 780',
-'IQ Anda Sebesar : 812',
-'IQ Anda Sebesar : 945',
-'IQ Anda Sebesar : 1000',
-'IQ Anda Sebesar : Tidak Terbatas!',
-'IQ Anda Sebesar : 5000',
-'IQ Anda Sebesar : 7500',
-'IQ Anda Sebesar : 10000',
-]
+    'Tu IQ es: 1',
+    'Tu IQ es: 14',
+    'Tu IQ es: 23',
+    'Tu IQ es: 35',
+    'Tu IQ es: 41',
+    'Tu IQ es: 50',
+    'Tu IQ es: 67',
+    'Tu IQ es: 72',
+    'Tu IQ es: 86',
+    'Tu IQ es: 99',
+    'Tu IQ es: 150',
+    'Tu IQ es: 340',
+    'Tu IQ es: 423',
+    'Tu IQ es: 500',
+    'Tu IQ es: 676',
+    'Tu IQ es: 780',
+    'Tu IQ es: 812',
+    'Tu IQ es: 945',
+    'Tu IQ es: 1000',
+    'Tu IQ es: ¡Infinito!',
+    'Tu IQ es: 5000',
+    'Tu IQ es: 7500',
+    'Tu IQ es: 10000'
+];
